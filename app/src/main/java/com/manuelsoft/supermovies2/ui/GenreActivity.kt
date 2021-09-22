6d0e9ea12b2102d86642c5094ad4c06d84164c6c
Victor Salazar <35509355+VictorSalazar777@users.noreply.github.com>
2021-09-22 14:55:35 -0500

    Add basic GenreActivity ui

--- app/src/main/java/com/manuelsoft/supermovies2/ui/GenreActivity.kt
+++ app/src/main/java/com/manuelsoft/supermovies2/ui/GenreActivity.kt
@@ -3,19 +3,32 @@ package com.manuelsoft.supermovies2.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.manuelsoft.supermovies2.R
+import com.manuelsoft.supermovies2.databinding.ActivityGenreBinding
+import com.manuelsoft.supermovies2.model.Genre
+import com.manuelsoft.supermovies2.model.PopularMoviesByGenre
 
 class GenreActivity : AppCompatActivity() {
 
     val TAG = GenreActivity::class.java.name
+    private lateinit var binding: ActivityGenreBinding
+    private lateinit var popularMovie: PopularMoviesByGenre
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_genre)
+        binding = ActivityGenreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val viewModel = Injector.getInjector(this).getGenreActivityViewModel(this)
         viewModel.popularMovies.observe(this, {
-            Log.d(TAG, it.toString())
+            popularMovie = it[0]
+            binding.apply {
+                tvMovieName.text = popularMovie.title
+                tvMovieOverview.text = popularMovie.overview
+                tvReleaseDate.text = popularMovie.releaseDate
+                tvOriginalLanguage.text = popularMovie.originalLanguage
+            }
         })
+
         viewModel.loadFavoriteMoviesByGenre(viewModel.loadGenre().id.toString())
     }
 }
