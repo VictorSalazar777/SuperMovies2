10d443397facecb231b27b1952fd1fdd4b7f19fc
Victor Salazar <35509355+VictorSalazar777@users.noreply.github.com>
2021-09-22 14:04:27 -0500

    Refactor MainActivity

--- app/src/main/java/com/manuelsoft/supermovies2/ui/MainActivity.kt
+++ app/src/main/java/com/manuelsoft/supermovies2/ui/MainActivity.kt
@@ -14,34 +14,56 @@ import com.manuelsoft.supermovies2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var rvGenresAdapter: RVGenresAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupToolbar()
+        createViewModel()
+        createRVGenresAdapter()
+        setupRvGenres()
+        setDataRVGenresAdapter()
+        setOpenFavorites()
+        setupBtnShow()
+    }
 
-        val rvGenresAdapter = RVGenresAdapter()
-        binding.rvGenres.apply {
-            layoutManager =
-                LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
-            adapter = rvGenresAdapter
+    private fun setupBtnShow() {
+        binding.btnShow.setOnClickListener {
+            viewModel.loadGenres()
         }
+    }
 
-        val viewModel = Injector.getInjector(this).getMainActivityViewModel(this)
-        viewModel.genres.observe(this, { t ->
-            rvGenresAdapter.setData(t)
-        })
+    private fun setOpenFavorites() {
         rvGenresAdapter.setOpenFavorites {
             val intent = Intent(this, GenreActivity::class.java)
             viewModel.saveGenre(it)
             startActivity(intent)
         }
+    }
 
-        binding.btnShow.setOnClickListener {
-            viewModel.loadGenres()
+    private fun setDataRVGenresAdapter() {
+        viewModel.genres.observe(this, { t ->
+            rvGenresAdapter.setData(t)
+        })
+    }
+
+    private fun setupRvGenres() {
+        binding.rvGenres.apply {
+            layoutManager =
+                LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+            adapter = rvGenresAdapter
         }
+    }
 
+    private fun createRVGenresAdapter() {
+        rvGenresAdapter = RVGenresAdapter()
+    }
+
+    private fun createViewModel() {
+        viewModel = Injector.getInjector(this).getMainActivityViewModel(this)
     }
 
     private fun setupToolbar() {
